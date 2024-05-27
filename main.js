bf3e19b05d4e9e3d7241c2870574e7df3702f9bf
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-27 07:55:57 +0200

    Created tailwind css configurations

---
+++ main.js
@@ -0,0 +1,5 @@
+// First declare all the varibales needed
+let addTaskInput = document.querySelector("#add-task"),
+  rmTaskInput = document.querySelector("#remove-task"),
+  tasksContainer = document.querySelector("#task-container"),
+  completedTasks = document.querySelector("#competed-tasks");
