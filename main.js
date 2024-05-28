f77295fe3addfb26ea35f53ed6a85ee26e1d8028
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-28 07:58:20 +0200

    added creating task function

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 // First declare all the varibales needed
 let addTaskInput = document.querySelector("#add-task"),
-  rmTaskInput = document.querySelector("#remove-task"),
+  rmTaskBtn = document.querySelector("#remove-task"),
+  newTask = document.querySelector("#new-task"),
   tasksContainer = document.querySelector("#task-container"),
-  completedTasks = document.querySelector("#competed-tasks");
+  completedTasks = document.querySelector("#competed-tasks"),
+  taskInfo = document.querySelector("#task-info");
+
+
+  // First add a function to create new task
+
+let createTask = (taskName) => {
+  let task = document.createElement("div");
+  let taskContent = `
+    
+    <p>${taskName}</p>
+    <input type="checkbox" name="done" >
+    
+    `;
+  task.classList.add("flex", "justify-between");
+  task.innerHTML = taskContent;
+  document.body.appendChild(task);
+};
