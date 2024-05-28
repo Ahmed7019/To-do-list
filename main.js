5942b99d453a918009610b307202852b600ac1f1
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-28 08:02:53 +0200

    button clicked and events

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ let addTaskInput = document.querySelector("#add-task"),
   newTask = document.querySelector("#new-task"),
   tasksContainer = document.querySelector("#task-container"),
   completedTasks = document.querySelector("#competed-tasks"),
-  taskInfo = document.querySelector("#task-info");
+  taskPopup = document.querySelector("#task-info"); // The task information popup
 
-
-  // First add a function to create new task
+// First add a function to create new task
 
 let createTask = (taskName) => {
   let task = document.createElement("div");
@@ -21,3 +20,14 @@ let createTask = (taskName) => {
   task.innerHTML = taskContent;
   document.body.appendChild(task);
 };
+
+// Events when clicking the button
+newTask.addEventListener("click", () => {
+  taskPopup.classList.remove("hidden");
+  taskPopup.classList.add("grid");
+});
+
+newTask.addEventListener("blur", () => {
+  taskPopup.classList.add("hidden");
+  taskPopup.classList.remove("grid");
+});
