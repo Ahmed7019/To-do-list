5ded84106ce8be730df51d27369822649b0b2d3c
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-06-05 10:35:18 +0300

    added new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
+        workSans: ["Work Sans", "sans-serif"],
         fontawesome: ['"Font Awesome 5 Free"'],
       },
       fontWeight: {
